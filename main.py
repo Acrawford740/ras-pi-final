0aa6f4c909ed644c13829e62b4b48ba741329887
Andrew-740 <crawfordandrew912@gmail.com>
2025-05-13 12:24:32 -0400

    feat: main

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-import gpiozero
+from gpiozero import LED, Button
+import time
+import random
+
+left_button = Button(24)
+right_button = Button(23)
+top_button = Button(25)
+bottom_button = Button(27)
+red_led = LED(26)
+yellow_led = LED(4)
+
+def loop():
+    pass
+
+def destroy():
+    left_button.close()
+    right_button.close()
+    top_button.close()
+    bottom_button.close()
+    red_led.close()
+    yellow_led.close()
