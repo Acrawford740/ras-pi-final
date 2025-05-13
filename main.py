423f95ef5d8dcd5594baf8ee7ae97446e47eb033
Andrew-740 <crawfordandrew912@gmail.com>
2025-05-13 12:27:32 -0400

    feat: main comments

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from gpiozero import LED, Button
 import time
 import random
 
+# Set The Buttons
 left_button = Button(24)
 right_button = Button(23)
 top_button = Button(25)
 bottom_button = Button(27)
+# Set The LEDs
 red_led = LED(26)
 yellow_led = LED(4)
 
+# Main Loop
 def loop():
     pass
 
+# Close All
 def destroy():
     left_button.close()
     right_button.close()
