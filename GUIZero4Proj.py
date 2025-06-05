c804ebdb4e714f7796779ee25642e3eb05248e3d
FMJack <hunterneely23@outlook.com>
2025-06-05 11:44:39 -0400

    fortnite

--- GUIZero4Proj.py
+++ GUIZero4Proj.py
@@ -1,10 +1,19 @@
-from guizero import App, Text, PushButton
+from guizero import App, Text, TextBox, PushButton
+
+correctPass = "80085"
 
 def passwordCheck():
-    if 
+    if textEntry.value == correctPass:
+        result.value = "Correct!"
+    else:
+        result.value = "Incorrect!"
 
-app = App(title = "Password Playtime!")
+app = App(title = "Password Playtime!",width=300, height=200)
 
-button2 = PushButton(app, command=passwordCheck)
+textEntry = TextBox(app, text="", hide_text=True)
 
-app.display
+checkButton = PushButton(app, text="Check Password", command=passwordCheck)
+
+result = Text(app,text="")
+
+app.display()
