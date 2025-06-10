842fa2e43cbf563e363fbe7f9617213a33785ed4
Andrew-740 <crawfordandrew912@gmail.com>
2025-06-10 10:31:32 -0400

    nods

--- testing.py
+++ testing.py
@@ -1,12 +1,24 @@
 from gpiozero import AngularServo
 import time
 
+GPIOside = 24
 GPIOup = 18
 SERVO_DELAY_SEC = 0.001
 myCorrection = 0.0
 maxPW = (2.5 + myCorrection)/1000
 minPW = (0.5 - myCorrection)/1000
 servoUp = AngularServo(GPIOup, initial_angle=0, min_angle=-180, max_angle=180, min_pulse_width = minPW, max_pulse_width = maxPW)
+servoSide = AngularServo(GPIOside, initial_angle=0, min_angle=-180, max_angle=180, min_pulse_width = minPW, max_pulse_width = maxPW)
+
+def nodNo():
+    timesNO = 0
+    while timesNO != 5:
+        times += 1
+        servoSide.angle = 40
+        time.sleep(1)
+        servoSide.angle = -40
+        time.sleep(1)
+    servoSide.angle = 0
 
 def nodYes():
     timesYes = 0
@@ -18,11 +30,17 @@ def nodYes():
         time.sleep(1)
     servoUp.angle = 5
 
+def main():
+    nodNo()
+    time.sleep(3)
+    nodYes()
+    time.sleep(3)
+
 def destroy():
     servoUp.close()
 
 if __name__ == "__main__":
     try:
-        nodYes()
+        main()
     except KeyboardInterrupt:
         destroy()
