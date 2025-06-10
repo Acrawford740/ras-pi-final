986e92cacbc8fe37279ab12575f6b6aeac4c0aa9
FMJack <hunterneely23@outlook.com>
2025-06-10 10:10:58 -0400

    the jiggle jiggle skin

--- testing.py
+++ testing.py
@@ -1,33 +1,33 @@
 from gpiozero import AngularServo
-from time import sleep
+import time
 
-GPIOside = 6
+GPIOside = 18
 SERVO_DELAY_SEC = 0.001
 myCorrection = 0.0
 maxPW = (2.5 + myCorrection)/1000
-minPW = (2.5 + myCorrection)/1000
-servoUp = AngularServo(GPIOup, initial_angle=0, min_angle=0, max_angle=360, min_pulse_width = minPW, max_pulse_width = maxPW)
+minPW = (0.5 - myCorrection)/1000
+servoSide = AngularServo(GPIOside, initial_angle=0, min_angle=-180, max_angle=180, min_pulse_width = minPW, max_pulse_width = maxPW)
 
 def nodNo():
-    for angle in range(0, 126, 1):
-        servoSide.angle = angle
-        time.sleep(SERVO_DELAY_SEC)
-    time.sleep(.5)
-    for angle in range(126, -40, -1):
-        servoSide.angle = angle
-        time.sleep(SERVO_DELAY_SEC)
-    time.sleep(.5)
+    times = 0
+    while times != 5:
+        times += 1
+        servoSide.angle = 40
+        time.sleep(1)
+        servoSide.angle = -40
+        time.sleep(1)
     servoSide.angle = 0
 
 def incorrect():
-    red_led.on()
-    playsound('sound-effects/extremely-loud-incorrect-buzzer_0cDaG20.mp3')
     nodNo()
     time.sleep(3)
-    red_led.off()
+
+def destroy():
+    servoSide.close()
+
 
 if __name__ == "__main__":
     try:
-        main()
+        incorrect()
     except KeyboardInterrupt:
         destroy()
