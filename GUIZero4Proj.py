969ca97bcf4cc22dfa29a4bdf76c51b239de2de7
FMJack <hunterneely23@outlook.com>
2025-06-10 13:17:23 -0400

    BUZZ BUZ

--- GUIZero4Proj.py
+++ GUIZero4Proj.py
@@ -1,14 +1,43 @@
-from guizero import App, Text, TextBox, PushButton
-from playsound import playsound
+from pygame import mixer
+from guizero import App,TextBox,PushButton,Text, Picture
+import time
 correctPass = "80085"
 
 def passwordCheck():
     if textEntry.value == correctPass:
         result.value = "Correct!"
-        playsound.playsound('.\sound-effects\loud-correct-buzzer.mp3')
+        rightbuzz()
     else:
         result.value = "Incorrect!"
-        playsound.playsound('.\sound-effects/extremely-loud-incorrect-buzzer_0cDaG20.mp3')
+        wrongpic = Picture(app, image="images/FuedX-removebg-preview.png", height=70, width = 50)
+        wrongbuzz()
+
+def wrongbuzz():
+# Starting the mixer
+        mixer.init()
+# Loading the song
+        mixer.music.load('/home/jack/CODE/ras-pi-final/sound-effects/incorrect_buzzer.mp3')
+
+# Setting the volume
+        mixer.music.set_volume(0.7)
+
+# Start playing the song
+        mixer.music.play()
+        time.sleep(2)
+
+def rightbuzz():
+# Starting the mixer
+        mixer.init()
+
+# Loading the song
+        mixer.music.load('/home/jack/CODE/ras-pi-final/sound-effects/loud-correct-buzzer.mp3')
+
+# Setting the volume
+        mixer.music.set_volume(0.7)
+
+# Start playing the song
+        mixer.music.play()
+        time.sleep(2)
 
 app = App(title = "Password Playtime!",width=300, height=200)
 
--- playsound.py
+++ playsound.py
@@ -1,13 +1,21 @@
-import playsound
+from pygame import mixer
 import time
 
 def noises():
     timesPlay = 0
     while timesPlay != 3:
         timesPlay += 1
-        playsound('sound-effects/extremely-loud-incorrect-buzzer_0cDaG20.mp3')
-        time.sleep(2)
-        playsound('sound-effects/loud-correct-buzzer.mp3')
+# Starting the mixer
+        mixer.init()
+
+# Loading the song
+        mixer.music.load('/home/jack/CODE/ras-pi-final/sound-effects/loud-correct-buzzer.mp3')
+
+# Setting the volume
+        mixer.music.set_volume(0.7)
+
+# Start playing the song
+        mixer.music.play()
         time.sleep(2)
 
 if __name__ == "__main__":
